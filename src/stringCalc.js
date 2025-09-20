8c0b58ffb9b15d067d4d410010b2028373204c64
tahas93 <tahas1993@gmail.com>
2025-09-20 20:56:50 +0530

    impl: support signle delimiter b/w numbers(pass)

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,8 +1,22 @@
+function escapeForRegex(s) {
+    // googled for regex to escape speical chars.
+    return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 function add(input) {
     if(!input) return 0;
-    if(!input.includes(',')) return parseInt(input, 10);
+    
+    let numPart = input;
+    let delimiterRegx = /,|\n/;
 
-    const eles = input.split(/,|\n/);
+    if(numPart.startsWith('//')) {
+        const newLineInd = numPart.indexOf('\n');
+        const def = numPart.substring(2, newLineInd);
+        numPart = numPart.substring(newLineInd + 1);
+        delimiterRegx = new RegExp(`${escapeForRegex(def)}|,|\\n`);
+    }
+
+    const eles = numPart.split(delimiterRegx);
     const nums = eles.map(i => parseInt(i,10));
     return nums.reduce((a,b) => a + b, 0);
 }
