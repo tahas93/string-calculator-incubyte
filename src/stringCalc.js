bdba31d376fbb48a93e7a9ab7a047bf3a1937864
tahas93 <tahas1993@gmail.com>
2025-09-20 20:31:29 +0530

    impl: two comma seperated numbers(pass)

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -1,6 +1,10 @@
 function add(input) {
-    if(input === '' || input == null) return 0;
-    return parseInt(input, 10);
+    if(!input) return 0;
+    if(!input.includes(',')) return parseInt(input, 10);
+
+    const eles = input.split(',');
+    const nums = eles.map(i => parseInt(i,10));
+    return nums.reduce((a,b) => a + b, 0);
 }
 
 module.exports = {add};
