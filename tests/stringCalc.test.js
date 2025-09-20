c0246334c93284f192cc2217e74e9fc49d3954f8
tahas93 <tahas1993@gmail.com>
2025-09-20 20:19:44 +0530

    test: signle number return value(fail)

--- tests/stringCalc.test.js
+++ tests/stringCalc.test.js
@@ -3,3 +3,7 @@ const {add} = require('../src/stringCalc');
 test('empty string returns 0', () => {
     expect(add('')).toBe(0);
 });
+
+test('signle number returns value', () => {
+    expect(add('1')).toBe(1);
+});
