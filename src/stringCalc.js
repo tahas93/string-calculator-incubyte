7e9de9f7701788e98f825ebe1915fce8b20abf53
tahas93 <tahas1993@gmail.com>
2025-09-20 20:40:22 +0530

    impl: new line between numbers(pass)

--- src/stringCalc.js
+++ src/stringCalc.js
@@ -2,7 +2,7 @@ function add(input) {
     if(!input) return 0;
     if(!input.includes(',')) return parseInt(input, 10);
 
-    const eles = input.split(',');
+    const eles = input.split(/,|\n/);
     const nums = eles.map(i => parseInt(i,10));
     return nums.reduce((a,b) => a + b, 0);
 }
